57a824482de26b3615461f966cadd60e3b18a890
Iván Corrales <developer@wesovi.com>
2016-09-02 07:47:24 +0200

    feat: removing error handler

--- lib/exkorpion.ex
+++ lib/exkorpion.ex
@@ -71,20 +71,7 @@ defmodule Exkorpion do
          scenarioType.(scenario[:do])
       rescue
         e in ExUnit.AssertionError -> 
-          header = Enum.join [__ENV__.module, ", line: ", __ENV__.line] 
-          errorMsg = Enum.join ["\n",header, "\n", elem(__ENV__.function,0)]
-          Logger.error errorMsg
-          Logger.error e.message
-          raise e
-
-        e in Exkorpion.Error.AssertionError -> 
-           
-          header = Enum.join [__ENV__.module, ", line: ", __ENV__.line] 
-          errorMsg = Enum.join ["\n",header, "\n", elem(__ENV__.function,0),e.message]
-          Logger.error errorMsg
-          raise e
-        
-        
+          raise e        
         e in BadFunctionError -> raise %Exkorpion.Error.InvalidStructureError{}
       end
     end
@@ -98,10 +85,6 @@ defmodule Exkorpion do
 
   end
 
-  
-  def should(operation, param1, param2) do
-    
-    Exkorpion.Should.should operation, param1, param2
-  end
+
 
 end
