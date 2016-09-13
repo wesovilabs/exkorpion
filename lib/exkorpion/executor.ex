85b4cc5ffbb58728e9c13c9588e4fd32791f1b75
Iván Corrales <developer@wesovi.com>
2016-09-13 07:35:55 +0200

    feat: fixing test

--- lib/exkorpion/executor.ex
+++ lib/exkorpion/executor.ex
@@ -3,13 +3,13 @@ defmodule Exkorpion.Executor do
 
   """
   
-  def run(context, with_, given_, when_, then_ ) when is_nil(with_) do
+  def run(context, with_, given_, when_, then_) when is_nil(with_) do
     sub_context = joinCtx(given_.(context),context)
     sub_context = joinCtx(when_.(sub_context),sub_context, context)
     then_.(sub_context)
   end
 
-  def run(context, with_, given_, when_, then_ ) do
+  def run(context, with_, given_, when_, then_) do
     Enum.each with_.(context), fn ctx ->
       sub_context = joinCtx(given_.(ctx),ctx)
       sub_context = joinCtx(when_.(sub_context),sub_context, ctx)
